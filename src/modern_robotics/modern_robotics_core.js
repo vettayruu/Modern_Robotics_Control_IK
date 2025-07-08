fad97a7c1886423c0319c2a6dafdeb00ac66c46e
LIU SONGTAO <135619114+vettayruu@users.noreply.github.com>
2025-07-08 10:06:08 +0900

    Update modern_robotics_core.js

--- src/modern_robotics/modern_robotics_core.js
+++ src/modern_robotics/modern_robotics_core.js
@@ -1452,7 +1452,7 @@ function JacobianSpace(Slist, thetalist) {
 function IKinBody(Blist, M, T, thetalist0, eomg, ev) {
     let thetalist = thetalist0.slice();
     let i = 0;
-    const maxiterations = 20;
+    const maxiterations = 50;
     let Tsb = FKinBody(M, Blist, thetalist);
     let Vb = se3ToVec(MatrixLog6(matDot(TransInv(Tsb), T)));
     let err = (Norm(Vb.slice(0, 3)) > eomg) || (Norm(Vb.slice(3, 6)) > ev);
@@ -1508,7 +1508,7 @@ function IKinBody(Blist, M, T, thetalist0, eomg, ev) {
 function IKinSpace(Slist, M, T, thetalist0, eomg, ev) {
     let thetalist = thetalist0.slice();
     let i = 0;
-    const maxiterations = 20;
+    const maxiterations = 50;
     let Tsb = FKinSpace(M, Slist, thetalist);
     let Vs = matDot(Adjoint(Tsb), se3ToVec(MatrixLog6(matDot(TransInv(Tsb), T))));
     let err = (Norm(Vs.slice(0, 3)) > eomg) || (Norm(Vs.slice(3, 6)) > ev);
